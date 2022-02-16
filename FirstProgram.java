fe25a7c52c5275f3354ee892c776f1df0a641688
Tyler Bulger <tybulger@gmail.com>
2022-02-16 16:00:53 -0500

    This should say Hello World

---
+++ FirstProgram.java
@@ -0,0 +1,20 @@
+//public is an access modifier
+//public - anyone has visibility to this class/method/variable
+//private
+//default
+//protect
+public class FirstProgram {
+    //state (variables)
+
+
+    //behaviors (methods)
+
+    //This is a main method
+    //It is the entry point of our entire application
+    public static void main(String[] args){
+
+        // what is print() - is a method that we are calling
+        // how do we call methods? - by adding parentheses ()
+        System.out.print("Hello World");
+    }
+}
